a736d01f87eceab831dc8d602e0bcc82d8cd7ca8
carpusher <carpusher.wu@gofreight.com>
2024-02-23 22:36:54 +0800

    enhance(pub): Remove unused role

--- publisher/main.tf
+++ publisher/main.tf
@@ -5,14 +5,14 @@ provider "aws" {
 module "eventbridge" {
   source = "terraform-aws-modules/eventbridge/aws"
 
-  bus_name = "POC-AUTO-my-bus"
-  role_name = "POC-AUTO-my-bus"
+  bus_name    = "POC-AUTO-my-bus"
+  create_role = false
 }
 
 resource "aws_mq_broker" "POC-AUTO-my-broker" {
-  broker_name = "POC-AUTO-my-broker"
-  engine_type = "RabbitMQ"
-  engine_version = "3.11.20"
+  broker_name        = "POC-AUTO-my-broker"
+  engine_type        = "RabbitMQ"
+  engine_version     = "3.11.20"
   host_instance_type = "mq.t3.micro"
   user {
     username = "hardcore"
